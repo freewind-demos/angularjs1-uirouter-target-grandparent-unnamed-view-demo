a59ca885d45af22f38d540d03a67897b2e458cb3
freewind <nowindlee@gmail.com>
2019-02-26 17:50:56 +0800

    demo

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
-AngularJS1 "@uirouter" Parent Child State Demo
-==============================================
+AngularJS1 "@uirouter" Target GrandeParent UnNamed View Demo
+============================================================
 
-当一个state的名字形如`a.b`的时候，它是state `a`的子state.
-
-当渲染`a.b`这个子state时，它会先渲染`a`这个父state，然后在`a`的template中包括的`<ui-view>`中，
-再继续渲染`a.b`对应的template。
-
-如果state `a`中的template中没有`<ui-view>`，那么`a.b`对应的template将无处渲染。
+一个unnamed的uiview，它的名字是`$default`
 
 ```
 npm install
--- app.js
+++ app.js
@@ -2,22 +2,31 @@ const app = angular.module('app', ['ui.router'])
 
 app.config(($stateProvider, $urlRouterProvider) => {
 
-  $stateProvider.state('parent', {
+  $stateProvider.state('grandparent', {
+    url: '/grandparent',
+    templateUrl: './templates/grandparent.html'
+  });
+
+  $stateProvider.state('grandparent.parent', {
     url: '/parent',
     templateUrl: './templates/parent.html'
   });
 
-  $stateProvider.state('parent.child', {
+  $stateProvider.state('grandparent.parent.child', {
     url: '/child',
     templateUrl: './templates/child.html'
   });
 
-  $stateProvider.state('child', {
-    url: '/child',
-    templateUrl: './templates/child.html'
+  $stateProvider.state('grandparent.parent.naughtyChild', {
+    url: '/naughty-child',
+    views: {
+      '$default@grandparent': {
+        templateUrl: './templates/child.html'
+      }
+    }
   });
 
-  $urlRouterProvider.otherwise('/parent');
+  $urlRouterProvider.otherwise('/grandparent');
 
 });
 
